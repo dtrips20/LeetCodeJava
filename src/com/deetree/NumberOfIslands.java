0919794f724d33ec3e37eb39e09095e33ab853e9
Deepak Tripathi <deepak_tripathi@symantec.com>
2019-10-11 09:24:42 -0700

    Added code for house robber

--- src/com/deetree/NumberOfIslands.java
+++ src/com/deetree/NumberOfIslands.java
@@ -23,20 +23,22 @@ public class NumberOfIslands {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++)
                 if (grid[i][j] == '1') {
-                    DFSMarking(grid, i, j);
-                    ++count;
+                    count = count + DFSMarking(grid, i, j);
+                    // ++count;
                 }
         }
         return count;
     }
 
-    private static void DFSMarking(char[][] grid, int i, int j) {
+    private static int DFSMarking(char[][] grid, int i, int j) {
         if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1')
-            return;
+            return 0;
         grid[i][j] = '0';
         DFSMarking(grid, i + 1, j);
         DFSMarking(grid, i - 1, j);
         DFSMarking(grid, i, j + 1);
         DFSMarking(grid, i, j - 1);
+
+        return 1;
     }
 }
