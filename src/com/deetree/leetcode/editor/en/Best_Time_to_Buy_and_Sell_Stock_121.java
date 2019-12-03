db41199421e1742df1e1a65654ccf5dcfe0d8395
Deepak Tripathi <deepaktripathi20@gmail.com>
2019-12-03 11:36:53 -0800

    Leetcode 121 - Best time to buy stock .

---
+++ src/com/deetree/leetcode/editor/en/Best_Time_to_Buy_and_Sell_Stock_121.java
@@ -0,0 +1,47 @@
+/*
+121. Best Time to Buy and Sell Stock
+====================================
+Easy
+====
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock),
+design an algorithm to find the maximum profit.
+
+Note that you cannot sell a stock before you buy one.
+
+Example 1:
+=========================
+Input: [7,1,5,3,6,4]
+Output: 5
+Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+             Not 7-1 = 6, as selling price needs to be larger than buying price.
+
+Example 2:
+========================
+Input: [7,6,4,3,1]
+Output: 0
+Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+package com.deetree.leetcode.editor.en;
+
+public class Best_Time_to_Buy_and_Sell_Stock_121 {
+
+    public static void main(String[] args) {
+
+        int result = new Best_Time_to_Buy_and_Sell_Stock_121().maxProfit(new int[]{7, 1, 5, 3, 6, 4});
+        System.out.println(result);
+
+    }
+
+    public int maxProfit(int[] prices) {
+        int curMax = 0;
+        int maxSoFar = 0;
+        for (int i = 1; i < prices.length; i++) {
+            curMax = Math.max(0, curMax + (prices[i] - prices[i - 1]));
+            maxSoFar = Math.max(maxSoFar, curMax);
+        }
+
+        return maxSoFar;
+    }
+}
